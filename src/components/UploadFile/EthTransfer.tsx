40cb1f40a77050cb897fedcb79c3cfb35654b4f6
Niklas <SchneiderNiklas97@web.de>
2022-05-20 22:36:58 +0200

    Fixed spelling mistake; Fixed HTML Change Bug

--- src/components/UploadFile/EthTransfer.tsx
+++ src/components/UploadFile/EthTransfer.tsx
@@ -1,195 +1,184 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import {ERC721TokenType, ETHTokenType, ImmutableXClient, Link} from "@imtbl/imx-sdk";
-import {TextField} from "@mui/material";
+import {
+  ERC721TokenType,
+  ETHTokenType,
+  ImmutableXClient,
+  Link,
+} from "@imtbl/imx-sdk";
+import { TextField } from "@mui/material";
 import "./styles.css";
 import Papa from "papaparse";
 
-
 interface PostData {
-    title: string;
-    body: string;
-    file: File | null;
+  title: string;
+  body: string;
+  file: File | null;
 }
 
 interface ImxProps {
-    walletAddress: string;
-    apiClient: ImmutableXClient;
-    imxLink: Link;
+  walletAddress: string;
+  apiClient: ImmutableXClient;
+  imxLink: Link;
 }
 
 export default function EthTransfer(props: ImxProps) {
-    const [formValues, setFormValues] = useState<PostData>({
-        title: "",
-        body: "",
-        file: null,
-    });
+  const [formValues, setFormValues] = useState<PostData>({
+    title: "",
+    body: "",
+    file: null,
+  });
 
-    const [EthTransferData, setEthTransferData] = useState([{
-        type: ETHTokenType.ETH,
-        amount: "",
-        toAddress: "",
-    }])
-    const addInput = () =>{
-        const updateDate2 = [...EthTransferData, {type: ETHTokenType.ETH, amount: "", toAddress:""}]
-        setEthTransferData(updateDate2)
-    };
+  const [EthTransferData, setEthTransferData] = useState([
+    {
+      type: ETHTokenType.ETH,
+      amount: "",
+      toAddress: "",
+    },
+  ]);
+  const addInput = () => {
+    const updateDate = [
+      ...EthTransferData,
+      { type: ETHTokenType.ETH, amount: "", toAddress: "" },
+    ];
+    setEthTransferData(updateDate);
+  };
 
-    const removeInput  = () =>{
-        EthTransferData.pop();
-        setEthTransferData([...EthTransferData]);
-    };
+  const removeInput = () => {
+    EthTransferData.pop();
+    setEthTransferData([...EthTransferData]);
+  };
 
-    const handleChange =(event: React.ChangeEvent<HTMLInputElement>) =>{
-
-        let updateData = [...EthTransferData]
-        for(let i= 0 ; i < updateData.length ; i++)
-        {
-            if (event.target.name === "wallet")
-            {
-                if(event.target.id === "Wallet-ID" + updateData[i].toAddress)
-                {
-                    updateData[i].toAddress = event.target.value
-                }
-            }
-            else if (event.target.name === "Etherium")
-            {
-                if (event.target.id === "Etherium-ID" + updateData[i].toAddress)
-                {
-                    updateData[i].amount = event.target.value
-                }
-            }
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let updateData = [...EthTransferData];
+    for (let i = 0; i < updateData.length; i++) {
+      if (event.target.name === "wallet") {
+        if (event.target.id === "Wallet-ID" + i) {
+          updateData[i].toAddress = event.target.value;
         }
+      } else if (event.target.name === "Ethereum") {
+        if (event.target.id === "Ethereum-ID" + i) {
+          updateData[i].amount = event.target.value;
+        }
+      }
+    }
 
-        setEthTransferData(updateData)
+    setEthTransferData(updateData);
+  };
 
-    };
-    //ToDo: Eindeutigkeit von toAddress?
-    const addInputElements = EthTransferData.map(({amount,toAddress})=>(
-        <div className="InputETH">
-            <TextField
-                id={"Wallet-ID" + toAddress}
-                label="Wallet-ID"
-                onChange={handleChange}
-                name= "wallet"
-                variant="outlined"
-                value={toAddress === "" ? "": toAddress}
+  const addInputElements = EthTransferData.map(
+    ({ amount, toAddress }, key: number) => (
+      <div className="InputETH">
+        <TextField
+          id={"Wallet-ID" + key}
+          label="Wallet-ID"
+          onChange={handleChange}
+          name="wallet"
+          variant="outlined"
+          value={toAddress === "" ? "" : toAddress}
+        />
 
-            />
-
-            <TextField
-                id={"Etherium-ID" + toAddress}
-                label="Etherium"
-                onChange={handleChange}
-                variant="outlined"
-                name= "Etherium"
-                value={amount === "" ? "": amount}
-
-            />
-
-        </div>
-
-    ))
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        <TextField
+          id={"Ethereum-ID" + key}
+          label="Ethereum"
+          onChange={handleChange}
+          variant="outlined"
+          name="Ethereum"
+          value={amount === "" ? "" : amount}
+        />
+      </div>
+    )
+  );
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => {
         // @ts-ignore
-        Papa.parse(event.target.files[0], {
-            header: true,
-            skipEmptyLines: true,
-            complete: results => {
-
-
-                // @ts-ignore
-                let data = results.data.map((d) => ({toAddress: d.toAddress, amount: d.amount, type: ETHTokenType.ETH}))
-                // @ts-ignore
-
-                setEthTransferData(data)
-
-            },
-        });
-        setFormValues((prevFormValues) => ({
-            ...prevFormValues,
-            file: event.target.files ? event.target.files[0] : null,
+        let data = results.data.map((d: any) => ({
+          toAddress: d.toAddress,
+          amount: d.amount,
+          type: ETHTokenType.ETH,
         }));
-    };
+        // @ts-ignore
 
+        setEthTransferData(data);
+      },
+    });
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      file: event.target.files ? event.target.files[0] : null,
+    }));
+  };
 
-    function transferEth() {
-        props.imxLink.transfer(EthTransferData)
+  function transferEth() {
+    props.imxLink.transfer(EthTransferData);
+  }
 
-    }
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    // Preventing the page from reloading
+    event.preventDefault();
 
-    const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-        // Preventing the page from reloading
-        event.preventDefault();
+    transferEth();
+  };
 
-        transferEth();
-    }
+  return (
+    <form onSubmit={submitForm}>
+      <div className="Uploader">
+        <div className="deposit-withdraw-section">
+          <h1>Ethereum Selection:</h1>
+          <div className="deposit-withdraw-group">
+            <TextField
+              id="outlined-basic"
+              label="Selected File: "
+              variant="outlined"
+              inputProps={{ readOnly: true }}
+              value={formValues.file?.name ?? "No File selected.."}
+            />
 
-    return (
-        <form onSubmit={submitForm}>
-            <div className="Uploader">
-                <div className="deposit-withdraw-section">
-                    <h1>Ethereum Selection:</h1>
-                    <div className="deposit-withdraw-group">
+            <Button size="large" variant="contained" component="label">
+              Upload File
+              <input
+                type="file"
+                onChange={handleFileChange}
+                accept=".csv"
+                hidden
+              />
+            </Button>
+          </div>
+          <div className="NFT-Items">
+            {addInputElements}
 
-                        <TextField
-                            id="outlined-basic"
-                            label="Selected File: "
-                            variant="outlined"
-                            inputProps={
-                                { readOnly: true, }}
-                            value={formValues.file?.name ?? "No File selected.."}
-                        />
+            <Button
+              size="large"
+              variant="contained"
+              component="label"
+              onClick={addInput}
+              id="AddItem"
+            >
+              Add Item
+            </Button>
 
-                        <Button size="large" variant="contained" component="label">
-                            Upload File
-                            <input type="file" onChange={handleFileChange} accept=".csv" hidden />
-                        </Button>
-                    </div>
-                    <div className="NFT-Items">
+            <Button
+              size="large"
+              variant="contained"
+              component="label"
+              onClick={removeInput}
+              id="RemoveItem"
+            >
+              Remove Item
+            </Button>
+          </div>
 
-
-                        {addInputElements}
-
-                        <Button
-                            size="large"
-                            variant="contained"
-                            component="label"
-                            onClick={addInput}
-                            id="AddItem"
-                        >
-                            Add Item
-                        </Button>
-
-                        <Button
-                            size="large"
-                            variant="contained"
-                            component="label"
-                            onClick={removeInput}
-                            id="RemoveItem"
-                        >
-                            Remove Item
-                        </Button>
-
-                    </div>
-
-                    <Box marginY={3}>
-                        <Button
-                            size="large"
-                            variant="contained"
-                            type= "submit"
-
-                        >
-                            Submit
-                        </Button>
-                    </Box>
-
-
-
-                </div>
-
-            </div>
-        </form>
-    );
+          <Box marginY={3}>
+            <Button size="large" variant="contained" type="submit">
+              Submit
+            </Button>
+          </Box>
+        </div>
+      </div>
+    </form>
+  );
 }
--- src/components/UploadFile/NftTransfer.tsx
+++ src/components/UploadFile/NftTransfer.tsx
@@ -2,226 +2,204 @@ import React, { useState } from "react";
 import Papa from "papaparse";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import {ERC721TokenType, ETHTokenType, ImmutableXClient, Link} from "@imtbl/imx-sdk";
-import {TextField} from "@mui/material";
+import { ERC721TokenType, ImmutableXClient, Link } from "@imtbl/imx-sdk";
+import { TextField } from "@mui/material";
 import "./styles.css";
 
-
 interface PostData {
-    title: string;
-    body: string;
-    file: File | null;
+  title: string;
+  body: string;
+  file: File | null;
 }
 
 interface ImxProps {
-    walletAddress: string;
-    apiClient: ImmutableXClient;
-    imxLink: Link;
+  walletAddress: string;
+  apiClient: ImmutableXClient;
+  imxLink: Link;
 }
 
 export default function BatchTransfer(props: ImxProps) {
+  const [formValues, setFormValues] = useState<PostData>({
+    title: "",
+    body: "",
+    file: null,
+  });
 
-    const [formValues, setFormValues] = useState<PostData>({
-        title: "",
-        body: "",
-        file: null,
+  const [allNftData, setAllNftData] = useState([
+    {
+      type: ERC721TokenType.ERC721, // Must be of type ERC721
+      tokenId: "", // the token ID
+      tokenAddress: "", // the collection address / contract address this token belongs to
+      toAddress: "", // the wallet address this token is being transferred to
+    },
+  ]);
+
+  const addInput = () => {
+    const updateData = [
+      ...allNftData,
+      {
+        type: ERC721TokenType.ERC721,
+        tokenId: "",
+        tokenAddress: "",
+        toAddress: "",
+      },
+    ];
+
+    // @ts-ignore
+    setAllNftData(updateData);
+  };
+
+  const removeInput = () => {
+    allNftData.pop();
+    setAllNftData([...allNftData]);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let updateData = [...allNftData];
+
+    for (let i = 0; i < updateData.length; i++) {
+      if (event.target.name === "wallet") {
+        if (event.target.id === "Wallet-ID" + i) {
+          updateData[i].toAddress = event.target.value;
+        }
+      } else if (event.target.name === "token") {
+        if (event.target.id === "Token-ID" + i) {
+          updateData[i].tokenId = event.target.value;
+        }
+      } else if (event.target.name === "contract") {
+        if (event.target.id === "Contract-ID" + i) {
+          updateData[i].tokenAddress = event.target.value;
+        }
+      }
+    }
+
+    setAllNftData(updateData);
+  };
+
+  const addInputElements = allNftData.map(
+    ({ tokenAddress, tokenId, toAddress }, key) => (
+      <div className="InputNFT">
+        <TextField
+          id={"Wallet-ID" + key}
+          label="Wallet-ID"
+          onChange={handleChange}
+          name="wallet"
+          value={toAddress === "" ? "" : toAddress}
+          variant="outlined"
+        />
+
+        <TextField
+          id={"Token-ID" + key}
+          label="NFT-Token"
+          onChange={handleChange}
+          variant="outlined"
+          name="token"
+          value={tokenId === "" ? "" : tokenId}
+        />
+
+        <TextField
+          id={"Contract-ID" + key}
+          label="Contract-ID"
+          onChange={handleChange}
+          variant="outlined"
+          name="contract"
+          value={tokenAddress === "" ? "" : tokenAddress}
+        />
+      </div>
+    )
+  );
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // @ts-ignore
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        // @ts-ignore
+        let data = results.data.map((d: any) => ({
+          tokenId: d.tokenId,
+          toAddress: d.toAddress,
+          tokenAddress: d.tokenAddress,
+          type: ERC721TokenType.ERC721,
+        }));
+        // @ts-ignore
+
+        setAllNftData(data);
+      },
     });
 
-    const [allNftData, setAllNftData] = useState([{
-        type: ERC721TokenType.ERC721, // Must be of type ERC721
-        tokenId: "", // the token ID
-        tokenAddress: "", // the collection address / contract address this token belongs to
-        toAddress: "", // the wallet address this token is being transferred to
-    }])
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      file: event.target.files ? event.target.files[0] : null,
+    }));
+  };
 
-    const addInput = () =>{
-        const updateData = [...allNftData, {type: ERC721TokenType.ERC721, tokenId: "", tokenAddress: "", toAdress: ""}]
+  function batchNftTransfer() {
+    props.imxLink.transfer(allNftData);
+  }
 
-        // @ts-ignore
-        setAllNftData(updateData)
+  const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    // Preventing the page from reloading
+    event.preventDefault();
 
+    batchNftTransfer();
+  };
 
-    };
-
-    const removeInput  = () =>{
-        allNftData.pop();
-        setAllNftData([...allNftData]);
-    };
-
-    const handleChange =(event: React.ChangeEvent<HTMLInputElement>) =>{
-
-       let updateData = [...allNftData]
-
-            for(let i= 0 ; i < updateData.length ; i++)
-            {
-                if (event.target.name === "wallet")
-                {
-                    if(event.target.id === "Wallet-ID" + updateData[i].tokenId)
-                    {
-                        updateData[i].toAddress = event.target.value
-                    }
-                }
-                else if (event.target.name === "token")
-                {
-                    if (event.target.id === "Token-ID" + updateData[i].tokenId)
-                    {
-                        updateData[i].tokenId = event.target.value
-                    }
-                }
-                else if (event.target.name === "contract")
-                {
-                    if (event.target.id === "Contract-ID" + updateData[i].tokenId)
-                    {
-                        updateData[i].tokenAddress = event.target.value
-                    }
-                }
-            };
-
-        setAllNftData(updateData)
-
-
-    };
-
-    //ToDo: Sind TokenIds immer eineindeutig ?
-
-    const addInputElements = allNftData.map(({tokenAddress,tokenId,toAddress})=>(
-        <div className="InputNFT">
+  return (
+    <form onSubmit={submitForm}>
+      <div className="Uploader">
+        <div className="deposit-withdraw-section">
+          <h1>NFT Selection:</h1>
+          <div className="deposit-withdraw-group">
             <TextField
-                id={"Wallet-ID" + tokenId}
-                label="Wallet-ID"
-                onChange={handleChange}
-                name= "wallet"
-                value={toAddress === "" ? "": toAddress}
-                variant="outlined"
-
+              id="outlined-basic"
+              label="Selected File: "
+              variant="outlined"
+              inputProps={{ readOnly: true }}
+              value={formValues.file?.name ?? "No File selected.."}
             />
 
-            <TextField
-                id={"Token-ID" + tokenId}
-                label="NFT-Token"
-                onChange={handleChange}
-                variant="outlined"
-                name= "token"
-                value={tokenId === "" ? "": tokenId}
+            <Button size="large" variant="contained" component="label">
+              Upload File
+              <input
+                type="file"
+                onChange={handleFileChange}
+                accept=".csv"
+                hidden
+              />
+            </Button>
+          </div>
+          <div className="NFT-Items">
+            {addInputElements}
 
-            />
+            <Button
+              size="large"
+              variant="contained"
+              component="label"
+              onClick={addInput}
+              id="AddItem"
+            >
+              Add Item
+            </Button>
 
-            <TextField
-                id={"Contract-ID" + tokenId}
-                label="Contract-ID"
-                onChange={handleChange}
-                variant="outlined"
-                name= "contract"
-                value={tokenAddress === "" ? "": tokenAddress}
+            <Button
+              size="large"
+              variant="contained"
+              component="label"
+              onClick={removeInput}
+              id="RemoveItem"
+            >
+              Remove Item
+            </Button>
+          </div>
 
-            />
-
+          <Box marginY={3}>
+            <Button size="large" variant="contained" type="submit">
+              Submit
+            </Button>
+          </Box>
         </div>
-
-    ))
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        // @ts-ignore
-        Papa.parse(event.target.files[0], {
-            header: true,
-            skipEmptyLines: true,
-            complete: results => {
-                // @ts-ignore
-                let data = results.data.map((d) => ({tokenId: d.tokenId, toAddress: d.toAddress, tokenAddress: d.tokenAddress, type: ERC721TokenType.ERC721}))
-                // @ts-ignore
-
-                setAllNftData(data)
-
-
-
-            },
-        });
-
-                setFormValues((prevFormValues) => ({
-            ...prevFormValues,
-            file: event.target.files ? event.target.files[0] : null,
-        }));
-    };
-
-
-    function batchNftTransfer() {
-
-        props.imxLink.transfer(allNftData);
-
-    }
-
-    const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-        // Preventing the page from reloading
-        event.preventDefault();
-
-        batchNftTransfer();
-
-    }
-
-
-    return (
-        <form onSubmit={submitForm}>
-        <div className="Uploader">
-            <div className="deposit-withdraw-section">
-                <h1>NFT Selection:</h1>
-                <div className="deposit-withdraw-group">
-
-                    <TextField
-                        id="outlined-basic"
-                        label="Selected File: "
-                        variant="outlined"
-                        inputProps={
-                            { readOnly: true, }}
-                        value={formValues.file?.name ?? "No File selected.."}
-                    />
-
-                    <Button size="large" variant="contained" component="label">
-                        Upload File
-                        <input type="file" onChange={handleFileChange} accept=".csv" hidden />
-                    </Button>
-                </div>
-                <div className="NFT-Items">
-
-
-                    {addInputElements}
-
-                    <Button
-                        size="large"
-                        variant="contained"
-                        component="label"
-                        onClick={addInput}
-                        id="AddItem"
-                    >
-                        Add Item
-                    </Button>
-
-                    <Button
-                        size="large"
-                        variant="contained"
-                        component="label"
-                        onClick={removeInput}
-                        id="RemoveItem"
-                    >
-                        Remove Item
-                    </Button>
-
-                </div>
-
-                <Box marginY={3}>
-                    <Button
-                        size="large"
-                        variant="contained"
-                        type= "submit"
-                    >
-                    Submit
-                    </Button>
-                </Box>
-
-
-
-            </div>
-
-        </div>
-        </form>
-    );
+      </div>
+    </form>
+  );
 }
